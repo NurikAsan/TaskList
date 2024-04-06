c5942f0f3a580a56706ecba21eb13890cb4ef045
Nurikov <asankylovnursultan@gmail.com>
2024-04-06 16:53:07 +0600

    Implemented authservice

--- src/main/java/com/nurikov/tasklist/service/impl/AuthServiceImpl.java
+++ src/main/java/com/nurikov/tasklist/service/impl/AuthServiceImpl.java
@@ -1,17 +1,37 @@
 package com.nurikov.tasklist.service.impl;
 
+import com.nurikov.tasklist.domain.user.User;
 import com.nurikov.tasklist.service.AuthService;
+import com.nurikov.tasklist.service.UserService;
 import com.nurikov.tasklist.web.dto.auth.JWTRequest;
 import com.nurikov.tasklist.web.dto.auth.JWTResponse;
+import com.nurikov.tasklist.web.security.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
+    private final AuthenticationManager authenticationManager;
+    private final UserService userService;
+    private final JwtTokenProvider jwtTokenProvider;
     @Override
     public JWTResponse login(JWTRequest loginRequest) {
-        return null;
+        JWTResponse jwtResponse = new JWTResponse();
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(),
+                loginRequest.getPassword()));
+        User user = userService.getByUsername(loginRequest.getUsername());
+        jwtResponse.setId(user.getId());
+        jwtResponse.setUsername(user.getUsername());
+        jwtResponse.setAccessToken(jwtTokenProvider.createAccessToken(user.getId(), user.getUsername(), user.getRole()));
+        jwtResponse.setRefreshToken(jwtTokenProvider.createRefreshToken(user.getId(), user.getUsername()));
+        return jwtResponse;
     }
 
     @Override
     public JWTResponse refresh(String refreshToken) {
-        return null;
+        return jwtTokenProvider.refreshUserToken(refreshToken);
     }
 }
