2edafb4b154efd2bf1d3d36a957bf7f1c9e6bcb7
Nurikov <asankylovnursultan@gmail.com>
2024-04-06 17:30:45 +0600

    Implemented user and task services

--- src/main/java/com/nurikov/tasklist/service/TaskService.java
+++ src/main/java/com/nurikov/tasklist/service/TaskService.java
@@ -9,6 +9,6 @@ public interface TaskService {
     Task getById(long id);
     List<Task> getAllByUserId(long userId);
     Task update(Task task);
-    Task create(Task task);
+    Task create(Task task, long userId);
     void delete(long id);
 }
--- src/main/java/com/nurikov/tasklist/service/impl/UserServiceImpl.java
+++ src/main/java/com/nurikov/tasklist/service/impl/UserServiceImpl.java
@@ -1,38 +1,68 @@
 package com.nurikov.tasklist.service.impl;
 
+import com.nurikov.tasklist.domain.exception.AccessDeniedException;
+import com.nurikov.tasklist.domain.exception.ResourceNotFoundException;
+import com.nurikov.tasklist.domain.user.Role;
 import com.nurikov.tasklist.domain.user.User;
+import com.nurikov.tasklist.repository.UserRepository;
 import com.nurikov.tasklist.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
 
 @Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
     @Override
     public User getById(long id) {
-        return null;
+        return userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User Not Found!"));
     }
 
     @Override
     public User getByUsername(String username) {
-        return null;
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found!"));
     }
 
+    @Transactional
     @Override
     public User update(User user) {
-        return null;
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.update(user);
+        return user;
     }
 
+    @Transactional
     @Override
     public User create(User user) {
-        return null;
+        if(userRepository.findByUsername(user.getUsername()).isPresent())
+            throw new IllegalStateException("User already exists");
+        if(!user.getPassword().equals(user.getPasswordConfirm()))
+            throw new IllegalStateException("Password incorrect");
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.create(user);
+        Set<Role> roles = Set.of(Role.ROLE_USER);
+        userRepository.insertUserRole(user.getId(), Role.ROLE_USER);
+        user.setRole(roles);
+        return user;
     }
 
     @Override
     public boolean isTaskOwner(long userId, long taskId) {
-        return false;
+        return userRepository.isTaskOwner(userId, taskId);
     }
 
+    @Transactional
     @Override
     public void delete(long id) {
-
+        userRepository.delete(id);
     }
 }
