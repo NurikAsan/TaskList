55d79d6d462a7ed122800a11310ca63d992e5232
Nurikov <asankylovnursultan@gmail.com>
2024-04-10 00:45:19 +0600

    Create TaskMapper

--- src/main/java/com/nurikov/tasklist/repository/TaskRepository.java
+++ src/main/java/com/nurikov/tasklist/repository/TaskRepository.java
@@ -3,6 +3,7 @@ package com.nurikov.tasklist.repository;
 
 
 import com.nurikov.tasklist.domain.task.Task;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,7 +11,7 @@ import java.util.Optional;
 public interface TaskRepository {
     Optional<Task> findById(Long id);
     List<Task> findAllByUserId(Long userId);
-    void assignToUserId(Long taskId, Long userId);
+    void assignToUserId(@Param("taskId") Long taskId,@Param("userId") Long userId);
     void update(Task task);
     void create(Task task);
     void delete(Long id);
