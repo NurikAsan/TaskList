5fb21d2033735d10885d9a1d64f2b47ffb53fd26
Nurikov <asankylovnursultan@gmail.com>
2024-04-13 00:39:36 +0600

    Implemented Jpa Entity

--- src/main/java/com/nurikov/tasklist/domain/task/Task.java
+++ src/main/java/com/nurikov/tasklist/domain/task/Task.java
@@ -1,15 +1,24 @@
 package com.nurikov.tasklist.domain.task;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Data
+@Entity
+@Table(name = "tasks")
 public class Task implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String title;
     private String description;
+
+    @Enumerated(EnumType.STRING)
     private Status status;
+
+    @Column(name = "expiration_date")
     private LocalDateTime expirationDate;
 }
