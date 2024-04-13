7a70391d55388535d212fa97a76836be8ad9dbd1
Nurikov <asankylovnursultan@gmail.com>
2024-04-14 00:49:22 +0600

    Fix bugs

---
+++ src/main/java/com/nurikov/tasklist/web/dto/task/TaskImageDTO.java
@@ -0,0 +1,11 @@
+package com.nurikov.tasklist.web.dto.task;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+public class TaskImageDTO {
+    @NotNull(message = "image must be not null!")
+    private MultipartFile multipartFile;
+}
